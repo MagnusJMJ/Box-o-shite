ee498b6c205aaaac3a4543f90f4fa0db02dff199
Magnus Johansen <magnus.jmj@gmail.com>
2017-03-23 17:09:31 +0100

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+for (i = 1; i < 101; i++) {
+  var fb = '';
+  if (i % 3 == 0) {
+    fb += 'Fizz';
+  }
+  if (i % 5 == 0) {
+    fb += 'Buzz';
+  }
+  if (fb == '') {
+    console.log(i);
+  } else {
+    console.log(fb);
+  }
+}
