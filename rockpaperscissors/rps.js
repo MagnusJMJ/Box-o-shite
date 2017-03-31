707fbb289b431eda98a305ab0f7848ec486de623
Magnus Johansen <magnus.jmj@gmail.com>
2017-03-31 20:13:34 +0200

    major rework

--- rockpaperscissors/rps.js
+++ rockpaperscissors/rps.js
@@ -1,69 +1,86 @@
-var myChoice = 0,
-    header = document.getElementById('header'),
-    ui = document.getElementsByClassName('init'),
-    button = document.getElementById('button'),
-    throbber = document.getElementById('throbber'),
+var myChoice,
+    oppChoice,
+    result,
+    initial  = document.getElementById('initialState'),
     rock = document.getElementById('rock'),
     paper = document.getElementById('paper'),
-    scissors = document.getElementById('scissors');
+    scissors = document.getElementById('scissors'),
+    resolved = document.getElementById('resolvedState'),
+    retry = document.getElementById('retryButton'),
+    throbber = document.getElementById('throbber');
 
 rock.addEventListener('click', choseRock);
 paper.addEventListener('click', chosePaper);
 scissors.addEventListener('click', choseScissors);
-button.addEventListener('click', reload);
+retry.addEventListener('click', tryAgain);
 
 function choseRock() {
   console.log('You chose rock.');
-  myChoice = 1;
-  fight()
+  myChoice = 0;
+  startGame();
 }
 function chosePaper() {
   console.log('You chose paper.');
-  myChoice = 2;
-  fight()
+  myChoice = 1;
+  startGame();
 }
 function choseScissors() {
   console.log('You chose scissors.');
-  myChoice = 3;
-  fight()
+  myChoice = 2;
+  startGame();
 }
 
-function fight() {
-  for (n = 0; n < ui.length; n++) {
-    ui[n].style.display = 'none';
-  }
+function startGame() {
+  initial.style.display = 'none';
   throbber.style.display = 'inline-block';
-
-  setTimeout(showResult, 1000);
-  function showResult() {
-    throbber.style.display = 'none';
-    switch(result) {
-      case 0:  header.innerHTML = "You lost!"; break;
-      case 1:  header.innerHTML = "You won!";  break;
-      default: header.innerHTML = "There was a draw!";
-    }
-    header.style.display = 'initial';
-    button.style.display = 'inline-block';
-
-    
-  }
-
-  var oppChoice = Math.ceil(Math.random() * 3);
-      oppWeapon = [null, 'rock.', 'paper.', 'scissors.'];
-  console.log('Opponent chose ' + oppWeapon[oppChoice]);
-
-  var result;
-  if (myChoice == oppChoice) {
-    console.log('There was a draw!');
-  } else if (myChoice == oppChoice + 1 || myChoice == oppChoice - 2) {
-    result = 1;
-    console.log('You won!');
-  } else {
-    result = 0;
-    console.log('You lost!');
-  }
+  setTimeout(resolve, 1000);
 }
 
-function reload() {
-  location.reload();
+function resolve() {
+  oppChoice = Math.round(Math.random() * 2);
+  var oc = ['rock.', 'paper.', 'scissors.'];
+  console.log('Opponent chose ' + oc[oppChoice]);
+
+  if (myChoice == oppChoice) {
+    console.log('You tied!');
+  } else if (myChoice == oppChoice + 1 || myChoice == oppChoice - 2) {
+    console.log('You won!');
+    result = 1;
+  } else {
+    console.log('You lost!');
+    result = 0;
+  }
+
+  showResult();
+}
+
+function showResult() {
+  var header = document.getElementById('resolvedHeader'),
+      myHand = document.getElementById('myChoice'),
+      oppHand = document.getElementById('oppChoice');
+
+  switch (result) {
+    case 1: header.innerHTML = 'You won!'; break;
+    case 0: header.innerHTML = 'You lost!'; break;
+    default: header.innerHTML = 'You tied!';
+  }
+
+  switch (myChoice) {
+    case 0: myHand.setAttribute('src', 'assets/rock.png'); break;
+    case 1: myHand.setAttribute('src', 'assets/paper.png'); break;
+    case 2: myHand.setAttribute('src', 'assets/scissors.png'); break;
+  }
+
+  switch (oppChoice) {
+    case 0: oppHand.setAttribute('src', 'assets/rock.png'); break;
+    case 1: oppHand.setAttribute('src', 'assets/paper.png'); break;
+    case 2: oppHand.setAttribute('src', 'assets/scissors.png'); break;
+  }
+
+  throbber.style.display = 'none';
+  resolved.style.display = 'inline-block';
+}
+
+function tryAgain() {
+  console.log('works');
 }
