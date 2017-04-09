590f0ecd47ca891c60c5662cef81b8cd90c3e50a
Magnus Johansen <magnus.jmj@gmail.com>
2017-04-09 10:18:23 +0200

    WIP cardgame

---
+++ cards.js
@@ -0,0 +1,16 @@
+var deck = {
+  hearts:   ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH'],
+  diamonds: ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD'],
+  clubs:    ['AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC'],
+  spades:   ['AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS'],
+  shuffle: function() {
+    for (suit in deck) {
+      for (i = suit.length - 1; i; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = suit[i];
+        suit[i] = suit[j];
+        suit[j] = temp;
+      }
+    }
+  },
+};
