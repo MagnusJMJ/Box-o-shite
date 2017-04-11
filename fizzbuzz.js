838193e2e1fe5a64a71a2148468ac3fbed7d368a
Magnus Johansen <magnus.jmj@gmail.com>
2017-04-11 09:50:47 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,6 @@
 for (i = 1; i < 101; i++) {
   var fb;
-  if (i % 3 == 0) {
-    fb += 'Fizz';
-  }
-  if (i % 5 == 0) {
-    fb += 'Buzz';
-  }
-  if (fb == '') {
-    console.log(i);
-  } else {
-    console.log(fb || i);
-  }
+  if (i % 3 == 0) { fb += 'Fizz'; }
+  if (i % 5 == 0) { fb += 'Buzz'; }
+  console.log(fb || i);
 }
