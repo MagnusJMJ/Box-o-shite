fb672f86e83d5497c8af0dee664c0967139d92d5
Magnus Johansen <magnus.jmj@gmail.com>
2017-04-27 11:54:57 +0200

    "updated

---
+++ bash_and_cmd/gitgud_v3.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# startup screen - all actions return to here when done
+startScreen()
+{
+  cd ~/Documents/GitHub || ( echo "ERROR: You've either moved or deleted the GitHub folder. Correct the path in the script." && exit )
+  clear
+  echo "****************************************"
+  echo "* Magnus' Mean Lean GitGud(tm) Machine *"
+  echo "****************************************"
+  printf "\n"
+
+  # lists subfolders (repositories) in the current folder
+  echo "REPOSITORIES:"
+  for folder in */; do
+    echo " * $folder"
+  done
+  printf "\n"
+
+  # lists possible commands
+  echo "FUNCTIONS:"
+  for option in "maker (UNIMPLEMENTED)" "cloner (UNIMPLEMENTED)" "pusher" "puller" "checker" "quit"; do
+    echo " * $option"
+  done
+  printf "\n"
+
+  echo "Commands go: [function] [repository] \"[message (for pushing)]\""
+  printf "\n"
+
+  #reads user input and sends it to the handler function
+  read -p "Type a command: " action repo msg
+  handler $action $repo $msg
+}
+
+handler()
+{
+  clear
+
+  # if you type 'all' as repository, will loop through all repositories performing the command
+  if [ $2 == "all" ]; then
+    for f in */; do
+      echo $f
+      cd $f
+      $1 $f $3
+      cd ..
+      printf "\n"
+    done
+  else
+    echo $2
+    cd $2 || ( read -t 5 -n 1 -p "Invalid repository! Returning to menu..." && startScreen )
+    $1 $2 $3
+    cd ..
+    printf "\n"
+  fi
+  read -r -n 1 -p "Action successfully performed! Press any key to return to menu..."
+  startScreen
+}
+
+maker()
+{
+  echo "UNIMPLEMENTED"
+}
+
+cloner()
+{
+  echo "UNIMPLEMENTED"
+}
+
+pusher()
+{
+  git add --all
+  git commit -m "$2" || git commit -m "Placeholder."
+  git push --all
+}
+
+puller()
+{
+  git pull
+}
+
+checker()
+{
+  git status
+}
+
+quit()
+{
+  exit
+}
+
+#Initializes startScreen
+startScreen
